cbb501b6b2d0daedd4506aa1325af7935f96e00d
Amar Patel <104065807+developeramar@users.noreply.github.com>
2026-06-27 13:29:11 +0530

    route issues fixed

--- backend/routes/gmailRoutes.js
+++ backend/routes/gmailRoutes.js
@@ -1,14 +1,11 @@
 const express = require("express");
-
 const router = express.Router();
 
-const auth = require("../middleware/auth"); // path check kar lena
+const auth = require("../middleware/authMiddleware");
 
 const {
     sendEmail
 } = require("../controllers/gmailController");
-
-console.log("GMAIL ROUTES LOADED");
 
 router.post(
     "/gmail/send",
